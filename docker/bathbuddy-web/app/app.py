59cdaf0b70f1e969df022e372b4edd4fc4be9ec8
Thomas Bruckmann <thomas.bruckmann@softgarden.de>
2022-05-03 11:59:20 +0200

    adds workaround for failing spotifyd

--- docker/bathbuddy-web/app/app.py
+++ docker/bathbuddy-web/app/app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 import spotipy
 import random
-import configparser, argparse, os
+import docker
 from spotipy.oauth2 import SpotifyOAuth
 from time import sleep
 from threading import Thread
@@ -13,8 +13,10 @@ app.config.from_pyfile('/app/settings.cfg')
 
 spotify = ""
 device_id = ""
+docker_client = ""
 scope = "user-read-playback-state,user-modify-playback-state,playlist-read-collaborative,playlist-read-private,streaming"
 isplaying = False
+devices = ""
 
 @app.route('/handwash')
 def handwash():
@@ -45,6 +47,13 @@ def toothbrush():
         return 'Playing music 2mins'
     return 'Music already playing'
 
+# Workaround because of an issue in librespot
+# https://github.com/Spotifyd/spotifyd/issues/903
+# https://github.com/librespot-org/librespot/issues/247
+@app.route('/check')
+def check():
+    return check_bathplayer(app.config["BATHPLAYER_NAME"])
+
 def create_spotify(app_id, app_secret, scope):
   global spotify
   auth_manager = SpotifyOAuth(client_id=app_id, client_secret=app_secret, redirect_uri="http://localhost:8888", scope=scope, open_browser=False)
@@ -53,12 +62,20 @@ def create_spotify(app_id, app_secret, scope):
 
   return auth_manager, spotify
 
-def fetch_bathplayer(devices, bathplayer_name):
+# Workaround because of an issue in librespot
+# https://github.com/Spotifyd/spotifyd/issues/903
+# https://github.com/librespot-org/librespot/issues/247
+def create_docker_client():
+  global docker_client
+  docker_client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+  return docker_client
+
+def fetch_bathplayer(bathplayer_name):
   global device_id
+  fetch_spotify_devices()
   for device in devices['devices']:
     if device['name'] == bathplayer_name:
       device_id = device['id']
-
 
 def playmusic(duration):
     global isplaying
@@ -96,10 +113,36 @@ def stopmusic():
     spotify.pause_playback(device_id=device_id)
     isplaying = False
 
+# Workaround because of an issue in librespot
+# https://github.com/Spotifyd/spotifyd/issues/903
+# https://github.com/librespot-org/librespot/issues/247
+
+def check_bathplayer(bathplayer_name):
+  global device_id
+  global devices
+  devices = spotify.devices()
+  for device in devices['devices']:
+    if device['name'] == bathplayer_name:
+      return "Spotifyd is running!"
+  return restart_spotifyd()
+
+def restart_spotifyd():
+  for container in docker_client.containers.list():
+    if "spotifyd" in container.attrs['Config']['Image']:
+      if "spotifyd" in container.attrs['Config']['Name']:
+        container.restart()
+        return "Spotifyd restarted"
+  return "Spotifyd not restarted"
+
+def fetch_spotify_devices():
+  global devices
+  devices = spotify.devices()
+
 if __name__ == '__main__':
     create_spotify(app.config["APP_ID"] , app.config["APP_SECRET"], scope)
+    create_docker_client()
+    fetch_spotify_devices()
 
-    devices = spotify.devices()
-    fetch_bathplayer(devices, app.config["BATHPLAYER_NAME"])
+    fetch_bathplayer(app.config["BATHPLAYER_NAME"])
 
     app.run(debug=True, host='0.0.0.0')
